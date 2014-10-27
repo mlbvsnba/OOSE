b9e2ccc9ec37e7846ffb574c7f74b42ea51b532a
Michael Stewart <mstewa34@jhu.edu>
2014-10-27 14:39:57 -0400

    Added basic models?

--- server/HiNote/models.py
+++ server/HiNote/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    creation_date = models.DateTimeField('date created')
+
+class Developer(User):
+    api_key = models.CharField(max_length=50)
