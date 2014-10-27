64f9ccf19505471b5b696a80c0be70f96f9827df
Michael Stewart <mstewa34@jhu.edu>
2014-10-27 16:57:27 -0400

    Added a few models and a many-to-one relationship (?)

--- server/HiNote/models.py
+++ server/HiNote/models.py
@@ -1,17 +1,43 @@
 from django.db import models
 
-# Create your models here.
 
 class User(models.Model):
     name = models.CharField(max_length=100)
     creation_date = models.DateTimeField('date created')
 
+
+class Subscription(models.Model):
+    # many-to-one relationship
+    owner = models.ForeignKey(Developer)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    creation_date = models.DateTimeField('date created')
+
+    def __init__(self, owner, name, description):
+        super(Subscription, self).__init__()
+        self.owner = owner
+        self.name = name
+        self.description = description
+
+
 class Developer(User):
     api_key = models.CharField(max_length=50)
-    
+
     def verify(self, api_key):
         """
-
-        :type api_key: string
+        Verifies a Developer's API key.
+        :param api_key string
         """
         return self.api_key == api_key
+
+    def create_subscription(self, name, description):
+        sub = Subscription(owner=self, name=name, description=description)
+        sub.save()
+
+    def push_notification(self, notification, subscription_key):
+        """
+        Pushes a given notification to the matching subscription.
+        :param notification: a DeveloperNotification to push
+        :param subscription_key: the integer key of the Developer's subscription to push to
+        """
+        pass
