c08fd129038143bbaa587e15b233057e70777c22
Michael Stewart <mstewa34@jhu.edu>
2014-10-31 14:33:39 -0400

    fixed long lines

--- server/HiNote/tests.py
+++ server/HiNote/tests.py
@@ -7,7 +7,9 @@ class DeveloperTestCase(TestCase):
 
     def test_creation(self):
         key = Developer.create_api_key()
-        dev = Developer.objects.create(api_key=key, username="dev1", password="dev1", email="dev1@dev1.com", first_name="John", last_name="Smith")
+        dev = Developer.objects.create(api_key=key, username="dev1",
+                                       password="dev1", email="dev1@dev1.com",
+                                       first_name="John", last_name="Smith")
         dev.save()
         dev_load = Developer.objects.get(api_key=key)
         self.assertIsNotNone(dev_load)
