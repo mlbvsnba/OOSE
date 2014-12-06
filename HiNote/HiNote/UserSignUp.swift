a3e400c4ea4410d9e86337bd6db4ebd74a2371ef
Cameron <coleary9@jhu.edu>
2014-12-05 19:04:00 -0500

    regi the registration

--- HiNote/HiNote/UserSignUp.swift
+++ HiNote/HiNote/UserSignUp.swift
@@ -17,17 +17,29 @@ class UserSignUp: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        let BOXHEIGHT: CGFloat = 40
         
-        let username_dialog_box = UITextField(frame: CGRectMake(self.view.frame.width*0.3, self.view.frame.height*0.3, 200, 100))
+        let username_banner = UILabel(frame: CGRectMake(self.view.frame.width*0.25, self.view.frame.height*0.2, self.view.frame.width*0.5, BOXHEIGHT))
+        username_banner.text = "Username:"
         
-        let password_dialog_box = UITextField(frame: CGRectMake(self.view.frame.width*0.3, self.view.frame.height*0.5, 200, 100))
+        let password_banner = UILabel(frame: CGRectMake(self.view.frame.width*0.25, self.view.frame.height*0.4, self.view.frame.width*0.5, BOXHEIGHT))
+        password_banner.text = "Password:"
+        
+        let username_dialog_box = UITextField(frame: CGRectMake(self.view.frame.width*0.25, self.view.frame.height*0.3, self.view.frame.width*0.5, BOXHEIGHT))
+        
+        let password_dialog_box = UITextField(frame: CGRectMake(self.view.frame.width*0.25, self.view.frame.height*0.5, self.view.frame.width*0.5, BOXHEIGHT))
         password_dialog_box.borderStyle = UITextBorderStyle.Line
         username_dialog_box.borderStyle = UITextBorderStyle.Line
         
-        let continue_to_stream = UIButton(frame: CGRectMake(self.view.frame.width*0.3, self.view.frame.height*0.8, 200, 100))
+        let continue_to_stream = UIButton(frame: CGRectMake(self.view.frame.width*0.25, self.view.frame.height*0.8, self.view.frame.width*0.5, BOXHEIGHT))
         continue_to_stream.addTarget(self, action: "pushNext", forControlEvents: UIControlEvents.TouchUpInside)
         continue_to_stream.setTitle("continue to rest of app", forState: UIControlState.Normal)
         continue_to_stream.setTitleColor(UIColor(white: 0, alpha: 1), forState: UIControlState.Normal)
+        
+        self.view.addSubview(username_banner)
+        
+        self.view.addSubview(password_banner)
+        
         self.view.addSubview(username_dialog_box)
         
         self.view.addSubview(password_dialog_box)
